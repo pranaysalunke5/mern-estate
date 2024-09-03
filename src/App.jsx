4cf6f90b79f2273aac7458b1b249059dd5a23fc0
Pranay <pranaysalunke5@gmail.com>
2024-09-03 13:09:32 +0530

    create pages,rotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 const App = () => {
   return (
-    <div className='flex text-red-500'>App</div>
+    <BrowserRouter>
+     <Routes>
+          <Route path="/" element={<Home/>}/> 
+          <Route path="/sign-in" element={<SignIn/>}/> 
+          <Route path="/about" element={<About/>}/> 
+          <Route path="/sign-up" element={<SignUp/>}/> 
+          <Route path="/profile" element={<Profile/>}/> 
+     </Routes>
+    </BrowserRouter>
   )
 }
 
