7df2c8bf3e04488e886a426053b70bb233726a05
Pranay <pranaysalunke5@gmail.com>
2024-09-21 12:34:38 +0530

    signin backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -15,3 +15,14 @@ export const signup = async (req,res,next) => {
   
   }
 }
+
+export const signin = async (req,res,next) => {
+  const {email,password} =req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next("user not found")
+      const validPassword = bcryptjs.compareSync(password,validUser.password)
+  }catch(error){
+    next(error)
+  }
+}
